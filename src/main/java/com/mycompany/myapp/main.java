f9a869886224cf0537f4d188c61b2b34e48a63ac
srinath-17 <85933521+srinath-17@users.noreply.github.com>
2023-04-28 12:40:45 +0530

    Update main.java

--- src/main/java/com/mycompany/myapp/main.java
+++ src/main/java/com/mycompany/myapp/main.java
@@ -1,1 +1,24 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@SpringBootApplication
+@Controller
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        // Validate the user's credentials and redirect to a success page
+        if (username.equals("admin") && password.equals("password")) {
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+}
